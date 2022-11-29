f496d40d7f68d38caafbbfa6f38327faa12da319
DmitriyS <eastmoscow@mail.ru>
2022-11-29 18:56:40 +0300

    sp

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,19 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+    @Bean
+    @Scope("prototype")
+    public Cat cat(){
+        return new Cat();
+    }
+}
